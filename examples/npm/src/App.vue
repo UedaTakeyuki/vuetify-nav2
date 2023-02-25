941fed9a609b1a3aa2b8b4a53af025a076c0d0d9
Takeyuki UEDA <ueda@latelierdueda.com>
2023-02-25 18:14:42 +0900

    add example of npm

---
+++ examples/npm/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <Navbar titleStr="Demo App" :links="links"/>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import {Navbar} from 'vuetify-nav2'
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  },
+  data: () => ({
+    links: [
+      { icon: 'home', text: 'Home', route: '/'},
+      { icon: 'info', text: 'About', route: '/about'},
+    ]
+  }),
+};
+</script>
