ca45372871b050894766842386d9239903c5b9a5
Takeyuki UEDA <ueda@latelierdueda.com>
2023-03-08 20:37:47 +0900

    test completed

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,24 +9,24 @@
         </v-toolbar-title>
       </slot>
       <slot name="menu">
-        <v-spacer></v-spacer>
+        <!--
+        <v-spacer></v-spacer>-->
 
-        <!-- dropdown menu -->
+        <!-- dropdown menu --><!--
         <v-menu offset-y>
           <template v-slot:activator="{ on }">
-          <!-- <v-btn text slot="activator"> -->
           <v-btn text v-on="on">
             <v-icon left>expand_more</v-icon>
             <span>Menu</span>
           </v-btn>
           </template>
           <v-list>
-            <!-- v-list-tile is changed to v-list-item -->
+            !!! v-list-tile is changed to v-list-item -!!!
             <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
               <v-list-item-title>{{ link.text }}</v-list-item-title>
             </v-list-item>
           </v-list>
-        </v-menu>
+        </v-menu>-->
       </slot>
     </v-app-bar>
 
---
+++ test/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <!-- Navigation bar -->
+    <Navbar titleStr="Demo App" :links="links"/>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+/* eslint-disable */
+import {Navbar} from '../../src/build-entry'
+import { mdiHome, /*mdiArrowLeft*/} from "@mdi/js";
+
+export default {
+  components: { Navbar},
+  data: () => ({
+    links: [
+      { icon: mdiHome, text: 'Home', route: '/'},
+      { icon: '', text: 'About', route: '/about'},
+    ]
+  }),
+  mounted(){
+    console.log("links",this.links)
+  }
+}
+</script>
