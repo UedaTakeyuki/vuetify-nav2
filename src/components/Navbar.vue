b3feeaf83d642dd7db65f6f271a8b3fbeb1e99d0
Takeyuki UEDA <ueda@latelierdueda.com>
2023-03-11 13:40:16 +0900

    v1.1.0

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -35,8 +35,13 @@
         <QRcode/>
         <!-- v-list-tile is changed to v-list-item -->
         <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
-          <v-list-item-action>
+          <v-list-item-action v-if="!link.svg">
             <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-item-action>
+
+          <v-list-item-action v-if="link.svg" class="white--text">
+            <!-- https://qiita.com/pentamania/items/e7a0f9d67ec75563b952 -->
+            <div :is="link.svg" style="width: 90%; height: 90%;"/>
           </v-list-item-action>
           <v-list-item-content>
             <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
--- test/src/App.vue
+++ test/src/App.vue
@@ -13,13 +13,14 @@
 import {Navbar} from '../../src/build-entry'
 //import {Navbar} from '../../dist/VuetifyNav2.umd'
 import { mdiHome, /*mdiArrowLeft*/} from "@mdi/js";
+import SvgIconMenu from './components/SvgIconMenu.vue'
 
 export default {
-  components: { Navbar},
+  components: { Navbar, SvgIconMenu},
   data: () => ({
     links: [
       { icon: mdiHome, text: 'Home', route: '/'},
-      { icon: '', text: 'About', route: '/about'},
+      { icon: '', text: 'About', route: '/about', svg: SvgIconMenu},
     ]
   }),
   mounted(){
